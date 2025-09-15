636f89cffdd248c733f4f60499bbc1a0695c6bf2
Johann Alberts <alberts.johann@gmail.com>
2025-09-15 22:34:05 +0100

    Create tokenizer class with encoder and decoder

--- tokenizer.py
+++ tokenizer.py
@@ -1,5 +1,26 @@
 import re
 
+
+class SimpleTokenizerV1:
+    def __init__(self, vocab):
+        self.str_to_int = vocab
+        self.int_to_str = {i:s for s,i in vocab.items()}
+
+    def encode(self, text):
+        preprocessed = re.split(r'([,.?_!"()\']|--|\s)', text)
+        preprocessed = [
+            item.strip() for item in preprocessed if item.strip()
+        ]
+        ids = [self.str_to_int[s] for s in preprocessed]
+        return ids
+
+    def decode(self, ids):
+        text = " ".join([self.int_to_str[i] for i in ids]) 
+
+        text = re.sub(r'\s+([,.?!"()\'])', r'\1', text)
+        return text
+
+
 def load_data():
   with open("data/verdict.txt", "r", encoding="utf-8") as f:
       raw_text = f.read()
@@ -29,7 +50,9 @@ if __name__ == "__main__":
   print_data_statistics(data)
   preprocessed = regex_tokenize(data)
   vocab = create_vocab(preprocessed)
-  for i, item in enumerate(vocab.items()):
-      print(item)
-      if i >= 50:
-          break
+  tokenizer = SimpleTokenizerV1(vocab)
+  text = """"It's the last he painted, you know," 
+       Mrs. Gisburn said with pardonable pride."""
+  ids = tokenizer.encode(text)
+  print(ids)
+  print(tokenizer.decode(ids))
