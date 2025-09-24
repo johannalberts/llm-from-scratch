5748bf0e5198fc8c3d67f67ddc1d8dd79b0791a0
Johann Alberts <alberts.johann@gmail.com>
2025-09-24 20:39:36 +0100

    Explore how attention is calculated step-by-step

--- exploring_attention.py
+++ exploring_attention.py
@@ -43,3 +43,45 @@ def softmax_naive(x):
 attn_weights_2_naive = softmax_naive(attn_scores_2)
 print("\nAttention weights:", attn_weights_2_naive)
 print("Sum:", attn_weights_2_naive.sum())
+
+#=========================
+# SELF-ATTENTION
+#=========================
+x_2 = inputs[1]     # The second input element
+d_in = inputs.shape[1]      # The input embedding size, d=3
+d_out = 2         # The output embedding size, d_out=2
+
+torch.manual_seed(123)
+W_query = torch.nn.Parameter(torch.rand(d_in, d_out), requires_grad=False)
+W_key   = torch.nn.Parameter(torch.rand(d_in, d_out), requires_grad=False)
+W_value = torch.nn.Parameter(torch.rand(d_in, d_out), requires_grad=False)
+print("\nW_query:\n", W_query)
+
+query_2 = x_2 @ W_query
+key_2   = x_2 @ W_key
+value_2 = x_2 @ W_value
+print("\nquery_2:\n", query_2)
+
+# Get key and value vectors for all inputs
+keys = inputs @ W_key 
+values = inputs @ W_value
+print("keys.shape:", keys.shape)
+print("values.shape:", values.shape)
+
+# Compute attention score between query_2 and all keys_2
+keys_2 = keys[1]
+attn_score_22 = query_2.dot(keys_2)
+print(attn_score_22)
+
+# Vectorized computation of attention scores between query_2 and all keys
+attn_scores_2 = query_2 @ keys.T       #1
+print(attn_scores_2)
+
+# Scaled dot-product attention weights
+d_k = keys.shape[-1]
+attn_weights_2 = torch.softmax(attn_scores_2 / d_k**0.5, dim=-1)
+print(attn_weights_2)
+
+# Compute context vector for input 2
+context_vec_2 = attn_weights_2 @ values
+print(context_vec_2)
