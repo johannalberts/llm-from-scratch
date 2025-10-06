cf4435fef4153b18dfcf34c90ac08ef927c44c9c
wondrousWebWorks <alberts.johann@gmail.com>
2025-10-06 17:17:42 +0100

    Use multi-head attention wrapper

--- attention.py
+++ attention.py
@@ -97,6 +97,21 @@ class CausalAttention(nn.Module):
         return context_vec
 
 
+class MultiHeadAttentionWrapper(nn.Module):
+    def __init__(self, d_in, d_out, context_length,
+                 dropout, num_heads, qkv_bias=False):
+        super().__init__()
+        self.heads = nn.ModuleList(
+            [CausalAttention(
+                 d_in, d_out, context_length, dropout, qkv_bias
+             ) 
+             for _ in range(num_heads)]
+        )
+
+    def forward(self, x):
+        return torch.cat([head(x) for head in self.heads], dim=-1)
+
+
 if __name__ == "__main__":
     d_in = inputs.shape[1]      # The input embedding size, d=3
     d_out = 2 
@@ -145,8 +160,19 @@ if __name__ == "__main__":
 
     torch.manual_seed(123)
     batch = torch.stack((inputs, inputs), dim=0)
-    print(batch.shape)  
-    context_length = batch.shape[1]
-    ca = CausalAttention(d_in, d_out, context_length, 0.0)
-    context_vecs = ca(batch)
+    # print(batch.shape)  
+    # context_length = batch.shape[1]
+    # ca = CausalAttention(d_in, d_out, context_length, 0.0)
+    # context_vecs = ca(batch)
+    # print("context_vecs.shape:", context_vecs.shape)
+
+    # Multi-head attention wrapper
+    context_length = batch.shape[1] # This is the number of tokens
+    d_in, d_out = 3, 2
+    mha = MultiHeadAttentionWrapper(
+        d_in, d_out, context_length, 0.0, num_heads=2
+    )
+    context_vecs = mha(batch)
+
+    print(context_vecs)
     print("context_vecs.shape:", context_vecs.shape)
